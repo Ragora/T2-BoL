e34945e797fc2fca47f412737d639e56928f0de9
Robert MacGregor <ragoradev@gmail.com>
2016-03-17 02:00:21 -0400

    Add DelDSO.sh script for Linux machines.

---
+++ DelDSO.sh
@@ -0,0 +1,15 @@
+// Shell Script Designed to remove .DSO files and any other temporary files
+
+rm -rf *.dso
+rm -rf *.cs~
+rm -rf *.txt~
+rm -rf *.gui~
+rm -rf *.mis~
+rm -rf *.cfg~
+rm -rf *.conf~
+rm -rf *.bat~
+rm -rf *.rtf~
+rm -rf *.db
+rm -rf *.sh~
+rm .gitignore~
+rm debugLog.txt
